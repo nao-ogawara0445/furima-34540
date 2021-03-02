35e5d6a03e8473e20e699cc81d0fa6e0d54df884
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-03-02 19:10:53 +0900

    マイグレート設定　カラムのみ

--- db/migrate/20210302070141_devise_create_users.rb
+++ db/migrate/20210302070141_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname          , null: false
+      t.string :name_sai          , null: false
+      t.string :name_mei          , null: false
+      t.string :name_huri_sai     , null: false
+      t.string :name_huri_mei     , null: false
+      t.date   :tanjyoubi         , null: false
 
       ## Recoverable
       t.string   :reset_password_token
