bc01c044a339511abd3aebbcc9cb61fc6001abe1
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-03-05 01:58:17 +0900

    選択肢の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
   has_one_attached :image
   has_one_attached :purchase
-  belongs_to :user
   
+  belongs_to :user
   validates :item_name , presence: true
   validates :item_exposition, presence: true
   validates :category_id, presence: true
   validates :postage_id  , presence: true
   validates :shipping_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true,format: { with:/\A[0-9]+\z/},numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :day_id, presence: true
   
   include ActiveHash::Associations
