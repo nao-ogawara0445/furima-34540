81ee72ed33a0338c5fde3a376ee54af9f5798d7b
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-03-04 17:01:54 +0900

    バリデーション設定

---
+++ db/migrate/20210304041748_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+
+      t.timestamps
+      t.string  :item_name 
+      t.text    :item_exposition
+      t.integer :category_id 
+      t.integer :status_id
+      t.integer :postage_id 
+      t.integer :shipping_id
+      t.integer :price  
+      t.integer :day_id 
+    end
+  end
+end
