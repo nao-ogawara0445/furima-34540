9ce1d46a6c06f77ec95fae3153e097441d43c5c3
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-03-05 17:36:56 +0900

    itamモデルにアクティブハッシュ紐付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,16 @@ class Item < ApplicationRecord
   validates :price,format: { with:/\A[0-9]+\z/},numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} 
   end
   with_options presence: true,numericality: {other_than: 1} do
-  validates :category_id  
-  validates :postage_id    
-  validates :status_id  
-  validates :shipping_id  
-  validates :day_id  
+  validates :category_id #カテゴリー 
+  validates :postage_id  #配送料
+  validates :status_id  #状態
+  validates :shipping_id #発送元
+  validates :day_id  #配送日
   end
-  include ActiveHash::Associations
-  has_many :articles
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :category
+  belongs_to :shipping
+  belongs_to :day
 end
