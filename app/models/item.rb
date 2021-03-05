a75449ae18b461eb2f5f30cc2bbad92f0865e092
nao-gabu0445 <78404803+nao-gabu0445@users.noreply.github.com>
2021-03-05 11:22:04 +0900

    単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ApplicationRecord
   has_one_attached :purchase
   
   belongs_to :user
+  validates :image, presence: true
   validates :item_name , presence: true
   validates :item_exposition, presence: true
   validates :category_id, presence: true
   validates :postage_id  , presence: true
+  validates :status_id  , presence: true
   validates :shipping_id, presence: true
   validates :price, presence: true,format: { with:/\A[0-9]+\z/},numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :day_id, presence: true
